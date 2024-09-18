22ad0e41d89fd1667b134a207168e85daf67752b
mtouphd <mtou.phd@gmail.com>
2024-09-17 21:34:48 -0400

    sale services

---
+++ oss-sales/src/main/java/org/mtou/java/oss/oss_sales/OssSalesApplication.java
@@ -0,0 +1,13 @@
+package org.mtou.java.oss.oss_sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OssSalesApplication.class, args);
+	}
+
+}
---
+++ oss-sales/src/test/java/org/mtou/java/oss/oss_sales/OssSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mtou.java.oss.oss_sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OssSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
